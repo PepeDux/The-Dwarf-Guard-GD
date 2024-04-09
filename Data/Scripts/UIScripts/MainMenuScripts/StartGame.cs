aef7ac575ef5eb14669a18709fa60573c734cd22
romanvitic@gmail.com <romanvitic@gmail.com>
2024-04-09 23:29:12 +0700

    Сделал меню паузы

---
+++ Data/Scripts/UIScripts/MainMenuScripts/StartGame.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class StartGame : TextureButton
+{
+	public override void _Ready()
+	{
+		// Событие на нажатие
+		Pressed += ButtonPressed;
+		// Событие на вход курсора на карту
+		MouseEntered += Entered;
+		// Событие на выход курсора на карту
+		MouseExited += Exited;
+	}
+
+	private void ButtonPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Data/Scenes/CoreScene/CoreScene.tscn");
+	}
+
+	private void Entered()
+	{
+		// Задаем курсору вид "лапки"
+		CursorStyleController.SetBeam();
+
+		Position = new Vector2(Position.X, Position.Y - 2);
+	}
+
+	private void Exited()
+	{
+		// Задаем курсору вид "стрелки"
+		CursorStyleController.SetArrow();
+
+		Position = new Vector2(Position.X, Position.Y + 2);
+	}
+}
---
+++ Data/Scripts/UIScripts/PauseMenuScripts/PauseMenu.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class PauseMenu : Node2D
+{
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("EscClick"))
+		{
+			if (Visible == true) 
+			{
+				Visible = false;
+				GetTree().Paused = false;
+			}
+			else if (Visible == false)
+			{
+				Visible = true;
+				GetTree().Paused = true;
+			}
+			
+		}
+	}
+}
