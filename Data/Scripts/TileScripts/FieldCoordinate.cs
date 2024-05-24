48de57c4ff0fe7a0c65577878539c3f70c237045
PepeDux <romanvitic@gmail.com>
2024-05-24 21:47:13 +0700

    Мелкие правки

--- Data/Scripts/TileScripts/FieldCoordinate.cs
+++ Data/Scripts/TileScripts/FieldCoordinate.cs
@@ -7,5 +7,5 @@ public partial class FieldCoordinate : Node
 	public const int yStartPoint = 17; //Стартовая координата по Y
 
 	public const int xFieldSize = 31; //Размер ширины игрового поля, начало отсчета с 0
-	public const int yFieldSize = 31; //Размер высоты игрового поля, начало отсчета с 0
+	public const int yFieldSize = 30; //Размер высоты игрового поля, начало отсчета с 0
 }
