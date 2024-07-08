68d73318e65982cd2cccf3b9d9c4becfba56f673
PepeDux <romanvitic@gmail.com>
2024-07-08 15:25:33 +0700

    Корректировки

--- Data/Scripts/ObjectsScripts/FunctionalObject/FunctionalObject.cs
+++ Data/Scripts/ObjectsScripts/FunctionalObject/FunctionalObject.cs
@@ -4,20 +4,20 @@ using System.Threading.Tasks;
 
 public partial class FunctionalObject : BaseObject
 {
-    public override Vector2I Coordinate
-    {
-        get
-        {
-            return coordinate;
-        }
-        set
-        {
-            coordinate = value;
-        }
-    }
+	public override Vector2I Coordinate
+	{
+		get
+		{
+			return coordinate;
+		}
+		set
+		{
+			coordinate = value;
+		}
+	}
 
 
-    public Character character;
+	public Character character;
 
 	public override void _Ready()
 	{
--- Data/Scripts/UIScripts/BaseUIElements/BaseProgressBar/BaseProgressBar.cs
+++ Data/Scripts/UIScripts/BaseUIElements/BaseProgressBar/BaseProgressBar.cs
@@ -4,13 +4,12 @@ using System;
 public partial class BaseProgressBar : TextureProgressBar
 {
 	public bool canChanged = true;
-	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready()
 	{
 		UpdateLabel();
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		UpdateLabel();
