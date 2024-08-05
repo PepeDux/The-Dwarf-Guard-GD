f1d298019e53caaf603a31d786133b1ea4893f48
Roman <romanvitic@gmail.com>
2024-08-05 13:19:09 +0700

    Исправил ошибку атаки игрока

--- Data/Scripts/AttackScripts/PlayerAttack.cs
+++ Data/Scripts/AttackScripts/PlayerAttack.cs
@@ -12,16 +12,11 @@ public partial class PlayerAttack : AttackScript
             GetParent<Player>().ActionPoints > 0 &&
             GetParent<Player>().canPerformAction)
         {
-            // Перебираем всех персонажей из хранилища персонажей
-            foreach (var target in CharacterStorage.characters)
-            {
-                // Проверяем, совпадает ли координата мыши с координатой цели и является ли цель врагом
-                if (MouseSelectTile.MouseCellPosition == target.Coordinate && target is Enemy)
-                {
-                    // Выполняем расчет атаки на цель
-                    CalculationAttack(target);
-                }
-            }
+            // Ищем цель для атаки в списке персонажей
+            Character target = CharacterStorage.characters.Where(target => MouseSelectTile.MouseCellPosition == target.Coordinate && target is Enemy).FirstOrDefault();
+
+            // Выполняем расчет атаки на цель
+            CalculationAttack(target);   
 
             // Запрещаем игроку выполнять действия
             GetParent<Player>().canPerformAction = false;
