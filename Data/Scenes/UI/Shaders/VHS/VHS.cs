d97adb064a2a403bdf235c5a427d2f817f4aaac7
Roman <romanvitic@gmail.com>
2024-09-09 20:38:50 +0700

    Скомпилировал все шейдеры в одну ноду

--- Data/Scenes/UI/Shaders/VHS/VHS.cs
+++ Data/Scenes/UI/Shaders/VHS/VHS.cs
@@ -5,12 +5,6 @@ public partial class VHS : Node2D
 {
 	ConfigFile config = new ConfigFile();
 
-	public override void _Ready()
-	{
-
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		config.Load("user://Settings.cfg");
